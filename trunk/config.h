e2933293a9a4067ae59b7364ed9b0171ed0a105a
lerosua <lerosua@044ec3d9-de3f-0410-b3bc-f5a4f4068c62>
2007-11-28 02:20:39 +0000

    add all file

---
+++ trunk/config.h
@@ -0,0 +1,20 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.in by autoheader.  */
+
+/* Define to 1 if you have the `socket' function. */
+#define HAVE_SOCKET 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "xihels@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "gmmlive"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "gmmlive 0.01"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "gmmlive"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.01"
