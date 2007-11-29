4b1013e414bc7ce04576c70228d8b0de4615236e
xihels <xihels@044ec3d9-de3f-0410-b3bc-f5a4f4068c62>
2007-11-29 11:06:07 +0000

    现在不会阻塞了

--- src/gmplayer.cpp
+++ src/gmplayer.cpp
@@ -11,9 +11,9 @@ GMplayer::GMplayer():
 	xid(-1),
 	timer(0)
 {
-//	Glib::signal_io().connect(
-//			sigc::mem_fun(*this, &GMplayer::on_callback),
-//			pst_ctrl.get_ptm(), Glib::IO_OUT);
+	Glib::signal_io().connect(
+			sigc::mem_fun(*this, &GMplayer::on_callback),
+			pst_ctrl.get_ptm(), Glib::IO_IN);
 
 }
 
@@ -23,11 +23,17 @@ GMplayer::~GMplayer()
 	stop();
 }
 
-bool GMplayer::on_callback(Glib::IOCondition condition)
+bool GMplayer::on_callback(const Glib::IOCondition& condition)
 {
 	char buf[256];
-	read(pst_ctrl.get_ptm(), buf, 256);
-		printf("%s", buf);
+	while (int len = read(pst_ctrl.get_ptm(), buf, 255)) {
+		if (len < 256) {
+			buf[len] = 0;
+			printf("%s", buf);
+			break;
+		}
+		printf("-%s", buf);
+	}
 	return true;
 }
 
