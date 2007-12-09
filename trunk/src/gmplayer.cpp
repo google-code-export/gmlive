f334d4d6013385cf0a95c3eaee28d8091263ba83
lerosua <lerosua@044ec3d9-de3f-0410-b3bc-f5a4f4068c62>
2007-12-09 12:11:20 +0000

    修正mms列表的堆栈错误

--- trunk/src/gmplayer.cpp
+++ trunk/src/gmplayer.cpp
@@ -2,7 +2,6 @@
 #include <gdk/gdkx.h>
 #include <sys/types.h>
 #include <signal.h>
-//#include <sstream>
 #include <sys/wait.h>
 #include "ec_throw.h"
 void GMplayer::set_s_pipe()
@@ -21,7 +20,6 @@ GMplayer::GMplayer(const sigc::slot<bool, Glib::IOCondition>& slot):
 	timer(0),
 	replay(false), 
 	child_call(slot)
-	//pst_ctrl(NULL)
 {
 	EC_THROW(-1 == pipe(stdin_pipe));
 	EC_THROW(-1 == pipe(stdout_pipe));
@@ -34,22 +32,6 @@ GMplayer::~GMplayer()
 }
 
 	
-//
-//bool GMplayer::on_callback(const Glib::IOCondition& condition)
-//{
-//	char buf[256];
-//	while (int len = read(pst_ctrl->get_ptm(), buf, 255)) {
-//		if (len < 256) {
-//			buf[len] = 0;
-//			printf("%s", buf);
-//			//parent->showMsg(buf);
-//			break;
-//		}
-//		printf("-%s", buf);
-//	}
-//	return true;
-//}
-
 void GMplayer::wait_mplayer_exit(GPid pid, int)
 {
 	if (childpid != -1) {
@@ -58,8 +40,6 @@ void GMplayer::wait_mplayer_exit(GPid pid, int)
 		waitpid(childpid, NULL, 0);
 		childpid = -1;
 
-		//delete pst_ctrl;
-		//pst_ctrl = NULL;
 
 		ready = true;
 	}
@@ -75,7 +55,6 @@ int GMplayer::my_system(char* const argv[])
 {
 	extern char **environ;
 
-	//pst_ctrl = new PstCtrl;
 
 	ptm_conn = Glib::signal_io().connect(
 			child_call,
@@ -89,8 +68,6 @@ int GMplayer::my_system(char* const argv[])
 
 
 		set_s_pipe();
-		//const char* argv[2] = {"mplayer", cmd.c_str()};
-		//execle("/usr/bin/mplayer", cmd.c_str(), environ);
 		execvp(argv[0], argv);
 
 		perror("mplayer execvp:");
@@ -107,17 +84,6 @@ bool GMplayer::is_runing()
 {
 	return (!ready) && (childpid > 0);
 }
-/*
-void GMplayer::stop()
-{
-	if (childpid > 0) {
-		kill(childpid, SIGKILL);
-		kill(childpid+1, SIGKILL);
-		childpid = -1;
-	}
-	ready = true;
-}
-*/
 
 void GMplayer::start()
 {
@@ -138,21 +104,14 @@ void GMplayer::start()
 	if(gwin)
 		xid=GDK_WINDOW_XID(gwin->gobj());
 
-	//char wid_buf[32];
-	//sprintf(wid_buf, "%d", xid);
-	char command_buf[64];
-	sprintf(command_buf, "mplayer -wid %d %s", xid, file.c_str());
+	char command_buf[256];
+	snprintf(command_buf,256, "mplayer -wid %d %s", xid, file.c_str());
 
 	const char* argv[5];
        	argv[0] = "sh";
 	argv[1] = "-c";
 	argv[2] = command_buf;
 	argv[3] = NULL;
-	//argv[0] = "mplayer";
-	//argv[1] = "-wid";
-	//argv[2] = wid_buf;
-	//argv[3] = file.c_str();
-	//argv[4] = NULL;
 
 	ready = false;
 	my_system((char* const *) argv);
@@ -171,10 +130,7 @@ void GMplayer::full_screen()
 
 void GMplayer::send_ctrl_word(char c)
 {
-	//if (pst_ctrl) {
-		//EC_THROW(-1 == write(pst_ctrl->get_ptm(), &c, sizeof (char)));
 		EC_THROW(-1 == write(stdin_pipe[1], &c, sizeof (char)));
-	//}
 }
 
 
