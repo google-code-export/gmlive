b81610a8c162735c782ec671ed05408ebaca47c7
xihels <xihels@044ec3d9-de3f-0410-b3bc-f5a4f4068c62>
2007-12-13 06:55:47 +0000

    修正了编译脚本

--- trunk/config.h
+++ trunk/config.h
@@ -62,13 +62,13 @@
 #define PACKAGE_NAME "gmlive"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "gmlive 0.01"
+#define PACKAGE_STRING "gmlive 0.11"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gmlive"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.01"
+#define PACKAGE_VERSION "0.11"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
