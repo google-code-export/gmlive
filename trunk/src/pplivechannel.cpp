57ccf577812d44f315a9b2d9145e224787d07dc9
xihels <xihels@044ec3d9-de3f-0410-b3bc-f5a4f4068c62>
2009-08-07 05:23:47 +0000

    修改了啥的日志

--- trunk/src/pplivechannel.cpp
+++ trunk/src/pplivechannel.cpp
@@ -80,7 +80,11 @@ void PpliveChannel::parse_channel(xmlNode* a_node)
 	Glib::ustring play_link = get_channel_item(cur_node, (const xmlChar*)"PlayLink");
 
 	Gtk::TreeModel::iterator piter = get_group_iter(catalog.c_str());
-	Gtk::TreeModel::iterator iter = m_liststore->append(piter->children());
+	Gtk::TreeModel::iterator iter = getListIter(piter->children(), channel_name);
+	if (iter != piter->children().end())
+		return;
+
+	iter = m_liststore->append(piter->children());
 
 	(*iter)[columns.name] = channel_name;
 
@@ -110,11 +114,17 @@ void PpliveChannel::parse_channels(xmlNode* a_node)
 
 bool PpliveChannel::read_channels(const char* filename)
 {
-	m_liststore->clear();
 	xmlDoc* doc = xmlReadFile(filename, NULL, 0); 
 	if (!doc) {
-		std::cerr << "pplive file error: " << filename << std::endl;
-		return false;
+		char command[1024];
+		snprintf(command, 1024, "iconv -f gbk -t utf8 %s | sed 's/encoding=\"gb2312\"/encoding=\"utf-8\"/' > /tmp/gmlive.xml.encode", filename);
+		printf("%s\n", command);
+		system(command);
+		doc= xmlReadFile("/tmp/gmlive.xml.encode", NULL, 0);
+		if (!doc) {
+			std::cerr << "pplive file error: " << filename << std::endl;
+			return false;
+		}
 	}
 
 	xmlNode* root_element = xmlDocGetRootElement(doc);
@@ -142,12 +152,15 @@ LivePlayer* PpliveChannel::get_player(const std::string& stream, TypeChannel pag
 	return PpLivePlayer::create(stream);
 }
 
-void PpliveChannel::refresh_list()
+void PpliveChannel::start_wget(const char* url)
 {
-	if (refresh)
+	if (!url){
+		signal_stop_refresh_.emit();
+		refresh = false;
 		return;
-	refresh = true;
-	signal_start_refresh_.emit();
+	}
+
+	std::cout<< url << std::endl;
 
 	int pid = fork();
 	if (pid == -1)
@@ -160,9 +173,9 @@ void PpliveChannel::refresh_list()
 		snprintf(buf, 512,"%s/.gmlive/pplive.lst.tmp",homedir);
 
 		const char* argv[6];
-       		argv[0] = "wget";
+		argv[0] = "wget";
 		//argv[1] = "http://list.pplive.com/zh-cn/xml/new.xml";
-		argv[1] = GMConf["pplive_channel_url"].c_str();
+		argv[1] = url;
 		argv[2] = "-O";
 		argv[3] = buf;
 		argv[4] = "-q";
@@ -176,6 +189,23 @@ void PpliveChannel::refresh_list()
 		(sigc::mem_fun(*this, &PpliveChannel::wait_wget_exit), pid);
 }
 
+void PpliveChannel::refresh_list()
+{
+	if (refresh)
+		return;
+
+	m_liststore->clear();
+
+	refresh = true;
+	signal_start_refresh_.emit();
+
+	const std::string& url =  GMConf["pplive_channel_url"];
+
+	memcpy(url_buf, url.c_str(), MIN(url.size(), 1024));
+	url_buf_cur = strtok(url_buf, "|");
+	start_wget(url_buf_cur);
+}
+
 void PpliveChannel::wait_wget_exit(GPid pid, int)
 {
 	if (wget_pid != -1) {
@@ -184,7 +214,7 @@ void PpliveChannel::wait_wget_exit(GPid pid, int)
 
 		refresh = false;
 	}
-	
+
 	char buf[512];
 	char buf2[512];
 	char* homedir = getenv("HOME");
@@ -204,7 +234,8 @@ void PpliveChannel::wait_wget_exit(GPid pid, int)
 		}
 	}
 
-	signal_stop_refresh_.emit();
+	url_buf_cur = strtok(NULL, "|");
+	start_wget(url_buf_cur);
 }
 
 
@@ -213,5 +244,5 @@ PpliveChannel::PpliveChannel(MainWindow* parent_) :
 	,wget_pid(-1)
 	,refresh(false)
 {
-	init();
+	//init();
 }
