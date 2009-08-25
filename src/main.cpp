d31863c0a045e2ef3b10bc1c19ee4974822ba09a
lerosua <lerosua@044ec3d9-de3f-0410-b3bc-f5a4f4068c62>
2009-08-25 13:53:41 +0000

    add single mode test

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,15 @@
  * =====================================================================================
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
 #include <iostream>
 #include "MainWindow.h"
 #ifdef HAVE_CONFIG_H
@@ -26,9 +35,35 @@
 using namespace std;
 TGMConf GMConf;
 
+void singleon()
+{
+	int sockfd,nbytes;
+	char buf[1024];
+	struct hostent *he;
+	struct sockaddr_in srvaddr;
+
+	if((sockfd=socket(AF_INET,SOCK_STREAM,0))==-1){
+		perror("socket error");
+		exit(1);
+	}
+	bzero(&srvaddr,sizeof(srvaddr));
+	srvaddr.sin_family=AF_INET;
+	srvaddr.sin_port=htons(GMPORT);
+	srvaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+
+	if(bind(sockfd,(struct sockaddr*)&srvaddr,
+			sizeof(struct sockaddr))==-1){
+		printf("there has another gmlive running\n");
+		exit(1);
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	setlocale (LC_ALL, "");
+
+	//保持只有一个gmlive实例
+	singleon();
 
 	bindtextdomain (GETTEXT_PACKAGE, GMLIVE_LOCALEDIR);
 	bind_textdomain_codeset (GETTEXT_PACKAGE, NULL);
